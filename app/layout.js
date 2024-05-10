7d866e8c99c3a22174dbf486bb2c0a647ff347a7
hhe107-SU <143746082+hhe107-SU@users.noreply.github.com>
2024-05-10 12:32:34 -0400

    final commit

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,22 @@
-// app/layout.js
+import Header from "../components/regions/Header";
+import Footer from "../components/regions/Footer";
+
 import "../sass/global.scss";
 
 export const metadata = {
  title: "IST 363 Spotify",
  description:
-  "A web application that showcases Top 10 lists, powered by WordPress and the Spotify API.",
+  "An application powered by Wordpress, React, Next.js and Spotify API",
 };
 
 export default function RootLayout({ children }) {
  return (
   <html lang="en">
-   <body>{children}</body>
+   <body>
+    <Header />
+    {children}
+    <Footer />
+   </body>
   </html>
  );
 }
